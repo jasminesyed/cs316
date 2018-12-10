9ed2a499e42d93a60bc4b0a5dbba1957467f1e97
jasminesyed <jasminessyed@gmail.com>
2018-12-10 12:13:12 -0500

    Add files via upload

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,24 +1,37 @@
 import React from 'react';
 
-let ImgVar = '';
+var image = {
+	backgroundImage: "url(assets/img/friends_coffee.jpg)",
+	backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overflow: 'auto',
+    height: 800,
+}
+var style1 = {
+  color: 'white',
+  fontSize: 25,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  fontFamily: 'Hoefler Text',
+  backgroundColor: 'transparent',
+  borderBottomWidth: 1,
+  borderLeftWidth: 0,
+  borderRightWidth:0,
+  borderTopWidth:0,
+  borderStyle: 'solid',
+  borderColor: 'white',
+  overflow: 'auto',
+  height: 750,
 
-let imgUrl = 'assets/img/friendscoffee.jpg'; 
-
-<div className = 'Component-Bg' 
-     style = {{ backgroundImage: 'url(' + imgUrl + ')', 
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center center',
-                backgroundRepeat: 'no-repeat',
-              }}>
-</div>
-
+};
 
 const HelloWorld = () => (
-  <p> We know that you are busy. Between going to classes, writing papers, and running to meetings, sometimes you need a coffee just to stay awake. That is why we created CoffeeBud, 
+<header style = {image}>
+	<p style = {style1}> We know that you are busy. Between going to classes, writing papers, and running to meetings, sometimes you need a coffee just to stay awake. That is why we created CoffeeBud, 
   a website that allows you to easily connect, deliver, and share coffee with your friends. Click on our logo to get started and create your own account today! </p>
-
+</header>
 );
-
+	
 
 
 export default HelloWorld;
