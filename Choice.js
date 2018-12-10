8186b58d2bc2aa880d491bb8ee5eccb1c38058a6
janelupica <45606742+janelupica@users.noreply.github.com>
2018-12-09 19:34:55 -0500

    Add files via upload

---
+++ Choice.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+//import { setInStorage, getFromStorage,} from '../../utils/storage';
+//import 'whatwg-fetch';
+var style1 = {
+  color: 'black',
+  fontSize: 40,
+  // textAlign: 'center',
+  // fontWeight: 900,
+  // fontFamily: 'Hoefler Text',
+  backgroundColor: 'tan',
+  textDecoration: 'none',
+  marginRight: 50,
+  marginLeft: 50,
+  marginBottom: 2000,
+};
+var container ={
+  display: 'flex',
+  // justifyContent: 'space-evenly',
+  alignItems: 'flex-end',
+  // backgroundColor: '#222',
+};
+
+
+class Choice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+
+};
+
+  render() {
+    let ImgVar = '';
+    var buttonStyle = {
+      borderRadius: 25,
+      labelColor: '#FFFFFF',
+      height: 60,
+      width: 200,
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'white',
+      border: 'none',
+      backgroundImage: 'url(' + 'assets/img/buttonC.jpg' + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      marginLeft: 800,
+      marginBottom: 50,
+    }
+
+    var desc = {
+      marginLeft: 800,
+      fontFamily: 'SignPainter',
+      fontSize : 55,
+      marginBottom: 10,
+      color: 'white',
+
+
+    }
+
+    return(
+        <div className='background-image' style ={ { backgroundImage: "url(assets/img/cafe.jpg)", backgroundSize: '100%', backgroundPosition: 'center', height: 575} }>
+        <img src={ImgVar} />,      
+        <div>
+        <p style = {desc}> Start a Coffee Run </p>
+        <button style = {buttonStyle}>
+          <Link to={{pathname:"/run", state: { token: this.props.token } }} style ={{textDecoration: 'none', color:'white'}} >Deliver</Link>
+        </button>
+        <div> 
+          <span>
+          </span>
+        </div>
+        <p style = {desc}> Find Me Coffee </p>
+        <button style = {buttonStyle}>
+         <Link to={{pathname:"/activeruns", state: { token: this.props.token }}} style ={{textDecoration: 'none', color:'white'}}>Order</Link>
+        </button>
+      </div>
+      </div>
+    )
+
+  }
+}
+
+export default Choice;
--- Header.js
+++ Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 var style1 = {
   color: 'white',
-  fontSize: 60,
+  fontSize: 65,
   textAlign: 'center',
   fontWeight: 900,
   fontFamily: 'Hoefler Text !important',
@@ -15,17 +15,18 @@ var style1 = {
   borderTopWidth:0,
   borderStyle: 'solid',
   borderColor: 'white',
-  overflow: 'none',
-  
+  overflow: 'auto',
+
 };
 var style2 = {
   color: 'white',
   fontSize: 20,
   textAlign: 'left',
   fontFamily: 'Josefin Sans !important',
-   backgroundColor: 'transparent',
+  backgroundColor: 'transparent',
   textDecoration:'none !important',
   overflow: 'auto',
+  marginTop: 10,
 
 };
 
@@ -33,13 +34,14 @@ var style2 = {
 const Header = () => (
   <header style = {style1}>
    <nav style = {style2}>
-      <Link to="/helloworld"className = "link" style={{ textDecoration: 'none' , color: 'white',   overflow: 'auto',
-}}>About</Link>
+      <Link to="/helloworld"className = "link" style={{ textDecoration: 'none' , color: 'white',   overflow: 'auto', fontSize: 25}}>About</Link>
     </nav>
-    <Link to="/" className = "link"style={{ textDecoration: 'none', color: 'white', shadowColor:'transparent', borderBottomWidth: 0,
-}}>COFFEEBUD</Link>
+    <Link to="/" className = "link"style={{ textDecoration: 'none', color: 'white', shadowColor:'transparent'}}>COFFEEBUD</Link>
     <hr />
   </header>
+ 
+
+      
 );
 
 
