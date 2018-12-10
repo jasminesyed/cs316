52e55ff1107739667386afc9f5777e948691e989
janelupica <45606742+janelupica@users.noreply.github.com>
2018-12-10 01:23:40 -0500

    Add files via upload

--- Header.js
+++ Header.js
@@ -9,13 +9,14 @@ var style1 = {
   fontFamily: 'Hoefler Text !important',
   backgroundColor: 'transparent',
   backgroundImage: "url(assets/img/c.jpg)",
-  borderBottomWidth: 1,
+  borderBottomWidth: 3,
   borderLeftWidth: 0,
   borderRightWidth:0,
   borderTopWidth:0,
   borderStyle: 'solid',
   borderColor: 'white',
   overflow: 'auto',
+  height : 120,
 
 };
 var style2 = {
@@ -29,7 +30,22 @@ var style2 = {
   marginTop: 10,
 
 };
+var buttonStyle = {
+      borderRadius: 25,
+      marginTop: 10,
+      labelColor: '#FFFFFF',
+      height: 30,
+      width: 100,
+      fontSize: 15,
+      fontWeight: 'bold',
+      color: 'white',
+      border: 'none',
+      backgroundImage: 'url(' + 'assets/img/buttonC.jpg' + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      overflow: 'auto',
 
+    }
 
 const Header = () => (
   <header style = {style1}>
