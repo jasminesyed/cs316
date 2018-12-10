438fac5f6892f5e47974893d1a7420b187ca39de
janelupica <45606742+janelupica@users.noreply.github.com>
2018-12-09 22:43:54 -0500

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+var image = {
+	backgroundImage: "url(assets/img/friendscoffee.jpg)",
+	backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overflow: 'auto',
+    height: 800,
+}
+var style1 = {
+  color: 'white',
+  fontSize: 25,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  fontFamily: 'Hoefler Text',
+  backgroundColor: 'transparent',
+  borderBottomWidth: 1,
+  borderLeftWidth: 0,
+  borderRightWidth:0,
+  borderTopWidth:0,
+  borderStyle: 'solid',
+  borderColor: 'white',
+  overflow: 'auto',
+  height: 750,
+
+};
+
+const HelloWorld = () => (
+<header style = {image}>
+	<p style = {style1}> We know that you are busy. Between going to classes, writing papers, and running to meetings, sometimes you need a coffee just to stay awake. That is why we created CoffeeBud, 
+  a website that allows you to easily connect, deliver, and share coffee with your friends. Click on our logo to get started and create your own account today! </p>
+</header>
+);
+	
+
+
+export default HelloWorld;
